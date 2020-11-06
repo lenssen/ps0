b8e0bff699de08bc760936781eda10f7e59a1de1
Lenssen <manasae.lenssen@gmail.com>
2020-11-06 19:18:22 +0530

    initial commit

---
+++ ps0.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  3 09:11:00 2020
+
+@author: Lenssen
+"""
+import numpy as np
+x = int(input('Enter number x: '))
+y = int(input('Enter number y: '))
+print('x**y = ',x**y)
+print('log(x) = ',int(np.log2(x)))
